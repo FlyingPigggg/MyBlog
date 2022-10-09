3d3b4681fa602028baa8c5eadf75e75919739e42
FlyingPigggg <1036634677@qq.com>
2022-10-10 01:21:08 +0800

    first commit

---
+++ sg-vue-admin/src/views/dashboard/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">
+      个人博客后台管理系统
+    </div>
+    <br />
+    <div>
+      有问题欢迎联系我的QQ：1036634677
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
